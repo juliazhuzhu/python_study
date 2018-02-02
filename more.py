8854509c3923e0f923fb9671a16348418b6d7334
xiaoye <3144605@qq.com>
2018-02-02 10:40:26 +0800

    add string cases

--- more.py
+++ more.py
@@ -1,7 +1,7 @@
 #/usr/bin/python
 """ split and interactively page a string or file of text 
 """
-
+import sys 
 def more(text, numlines=15):
 
 	lines = text.splitlines() 
@@ -13,8 +13,46 @@ def more(text, numlines=15):
 		if lines and input('More?') not in ['y', 'Y']: 
 			break
 
+def string_test():
+	myStr = 'SpurnYou'
+	if 'Sprn' in myStr:
+		print ('yes')
+	else:
+		print ('no')
+
+	#find is case sensitive
+	if (myStr.find('You') != -1):
+		print ('yes')
+	else:
+		print ('no')
+	
+	print (myStr.upper())
+	print (myStr.lower())
+	print (myStr.capitalize())
+
+	testStr = 'xxSpurnxxFuck'
+	splitStr = testStr.split('xx')
+	print(splitStr)
+	print('mlgb'.join(splitStr))
+
+	print(int("57") + 1)
+	print('{:d}'.format(558))
+	print( str(32) + repr(43))
+
+	file = open('spurn.txt', 'w')
+	file.write(str("fuck you")*5 + '\n')
+	file.close()
+
+	file = open('spurn.txt', 'r')
+	text = file.read()
+
+	file.close()
+	print (text[:])
+
+	print (sys.platform[:])
 if __name__ == '__main__':
-	import sys 
-	more(open(sys.argv[1]).read(), 10)
+	
+	#more(open(sys.argv[1]).read(), 10)
+	string_test()
 
 # when run, not imported # page contents of file on cmdline
